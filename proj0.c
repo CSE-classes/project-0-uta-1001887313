5aeee492083ffff905412e38a2a728e80e502ea1
uta-1001887313 <exr7313@mavs.uta.edu>
2025-09-23 21:06:56 -0500

    Add fixed string

--- proj0.c
+++ proj0.c
@@ -6,8 +6,8 @@ int
  main(int argc, char *argv[])
 {
   int i;
-  char str[]="CSE ";
-  printf(1,"%s\n",str);
+  char str[]="CSE3320 proj0 printing in user space: ";
+  printf(1,"%s",str);
   for(i = 1; i < argc; i++)
     printf(1, "%s%s", argv[i], i+1 < argc ? " " : "\n");
   exit();
